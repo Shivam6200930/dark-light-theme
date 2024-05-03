b7806514458421682d50b03f57229a99ebd35930
shivam <mandjoai123@gmail.com>
2024-05-03 16:01:39 +0530

    Initial commit

---
+++ src/contexts/theme.js
@@ -0,0 +1,17 @@
+import { createContext,useContext } from "react";
+
+export const themeContext = createContext({
+    themeMode:"Light",
+    darkTheme:()=>{
+
+    },
+    lightTheme:()=>{
+
+    },
+})
+
+export const ThemeProvider = themeContext.Provider
+
+export default function useTheme(){
+    return useContext(themeContext)
+}
